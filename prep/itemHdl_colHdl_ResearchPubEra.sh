1d43e1923aa139d13183c55edabc0046b256ab6e
grantj-re3 <grantj-re3@github.fake>
2014-04-10 12:20:47 +0930

    Added itemHdl_colHdl_ResearchPubEra.sh to report on items in the ERA 2010 tree.

--- prep/itemHdl_colHdl_ResearchPubEra.sh
+++ prep/itemHdl_colHdl_ResearchPubEra.sh
@@ -13,17 +13,23 @@ era_year_comm_name_regex='^ERA 2010$'
 sql="
 select
   i.item_id,
+  i.owning_collection c_owner,
+  (select substring(name,1,10) from collection where collection_id=i.owning_collection) c_owner_name,
+  array_to_string(array(
+      select substring(name,1,10) from collection2item c2i2, collection c2 where c2i2.item_id=i.item_id and
+      c2i2.collection_id <> i.owning_collection and c2i2.collection_id=c2.collection_id order by name
+  ), '||') c_others_name,
+
   (select text_value from metadatavalue where item_id=i.item_id and metadata_field_id=
     (select metadata_field_id from metadatafieldregistry where element='identifier' and qualifier='rmid')
   ) rmid,
   (select handle from handle where resource_id=i.item_id and resource_type_id=2) item_hdl,
-  (select count(*) from bitstream where deleted='f' and bitstream_id in
-    (select bitstream_id from bundle2bitstream where bundle_id =
-      (select bundle_id from bundle where name='ORIGINAL' and bundle_id in 
-        (select bundle_id from item2bundle where item_id=i.item_id)
-      )
-    )
-  ) num_files
+  (select handle from handle where resource_id=i.owning_collection and resource_type_id=3) c_owner_hdl,
+  array_to_string(array(
+      select handle from collection2item c2i2, handle where c2i2.item_id=i.item_id and
+      c2i2.collection_id <> i.owning_collection and c2i2.collection_id=resource_id and
+      resource_type_id=3 order by handle_id
+  ), '||') c_others_hdl
 from
 (
   select
@@ -56,9 +62,11 @@ with
   delimiter ','
   csv
     header
+    force quote c_owner_name, c_others_name
 "
 
-descr="Count the number of files for each item:
+descr="Show the RMID, item-handle, owner-handle, list of
+collection-handles and miscellaneous information for each item:
 - within each collection
 - under each sub-community
 - under the communities which match the regex /$era_year_comm_name_regex/
@@ -74,7 +82,6 @@ NOTES:
   reporting-year.
 
 3) Items 8202,8206 have no RMID (so I presume they did not originate from RM).
-
 "
 
 ##############################################################################

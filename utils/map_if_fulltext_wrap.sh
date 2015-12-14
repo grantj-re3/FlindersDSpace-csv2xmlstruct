c0b9912ad27eab3ed0b5b91593406ac62827d4bd
grantj-re3 <grantj-re3@github.fake>
2015-12-15 10:29:11 +1030

    Insert DEST_REPORT as an attachment instead of inserting into the email body.
    Reason: If any item title within DEST_REPORT contains one or more
    special/foreign character, the whole email body is blank!

--- utils/map_if_fulltext_wrap.sh
+++ utils/map_if_fulltext_wrap.sh
@@ -23,6 +23,7 @@ MK_BMET_CSV_CMD=$BASE_DIR/utils/map_if_bitstream.rb
 
 DEST_DIR=$BASE_DIR/map_if_fulltext
 DEST_REPORT=$DEST_DIR/map_report.$TIMESTAMP.txt
+DEST_REPORT_HEADER=$DEST_DIR/map_report_hdr.$TIMESTAMP.txt
 DEST_CSV=$DEST_DIR/map_bmet.$TIMESTAMP.csv
 ERROR_LOG=$DEST_DIR/map_bmet.$TIMESTAMP.err
 IMPORT_LOG=$DEST_DIR/map_bmet.$TIMESTAMP.log
@@ -44,12 +45,16 @@ email_exit_on_error() {
   error_type="$2"
 
   if [ $error_code != 0 ]; then
+    opts_attachments="-a $DEST_REPORT"
+    [ -f $IMPORT_LOG ] && opts_attachments="$opts_attachments -a $IMPORT_LOG"
     (
       echo "$REF"
-      echo "$error_type ERROR = $error_code" |tee $ERROR_LOG
-      cat $DEST_REPORT
-      [ -f $IMPORT_LOG ] && (echo; cat $IMPORT_LOG)
-    ) | mailx -s "$EMAIL_SUBJECT_ERROR" $EMAIL_DEST_LIST
+      echo
+      echo "$error_type **ERROR** = '$error_code'" |tee $ERROR_LOG
+      echo "The items might not be mapped to the destination collection"
+
+      cat $DEST_REPORT_HEADER
+    ) | mailx $opts_attachments -s "$EMAIL_SUBJECT_ERROR" $EMAIL_DEST_LIST
     exit $error_code
   fi
 }
@@ -59,16 +64,24 @@ email_exit_on_error() {
 ##############################################################################
 [ ! -d $DEST_DIR ] && mkdir $DEST_DIR
 $MK_BMET_CSV_CMD > $DEST_CSV 2> $DEST_REPORT	# Create BMET CSV file
-email_exit_on_error $? Ruby
+res=$?
+awk '
+  BEGIN {is_start=1}
+  /^ITEMS BEING MAPPED/ {is_start=0}
+  is_start==1 {print}
+' $DEST_REPORT > $DEST_REPORT_HEADER
+
+email_exit_on_error $res Ruby
 
 if [ `wc -l < $DEST_CSV` -ge 2 ]; then		# At least 1 item to import
   $IMPORT_CMD $DEST_CSV 2>&1 > $IMPORT_LOG	# Perform BMET import
   email_exit_on_error $? Import
 fi
 
+opts_attachments="-a $DEST_REPORT"
+[ -f $IMPORT_LOG ] && opts_attachments="$opts_attachments -a $IMPORT_LOG"
 (
   echo "$REF"
-  cat $DEST_REPORT
-  [ -f $IMPORT_LOG ] && (echo; cat $IMPORT_LOG)
-) |mailx -s "$EMAIL_SUBJECT" $EMAIL_DEST_LIST	# Email a report
+  cat $DEST_REPORT_HEADER
+) |mailx $opts_attachments -s "$EMAIL_SUBJECT" $EMAIL_DEST_LIST	# Email a report
 
